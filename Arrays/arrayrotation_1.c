5f3c10ef872647805ca16e9be6a495705f003337
Nishith Goswami <nishith2121@gmail.com>
2020-05-01 21:35:19 +0530

    Updated comments

--- Arrays/arrayrotation_1.c
+++ Arrays/arrayrotation_1.c
@@ -4,31 +4,16 @@
 #include<stdio.h>
 
 void rotate_array(int *array,unsigned int d,unsigned int len);
+void print_array(int *array,unsigned int len);
 
 int main()
 {
 	int array[] = {1,2,3,4,5,6,7};
 	unsigned int i,d;
-	d=2;
 	unsigned int len = sizeof(array)/sizeof(array[0]);
-	
-	printf("\n# Original Array\n");
-	for(i=0;i<len;i++)
-	{
-		printf("%d ",array[i]);		
-	}
-	printf("\n");
-	
-	printf("#After %d rotation\n",d);
-	rotate_array(array,d,len-1);
-	for(i=0;i<len;i++)
-	{
-		printf("%d ",array[i]);		
-	}
-	printf("\n");
+	rotate_array(array,2,len);
+	print_array(array,len);
 	printf("#Time Complexity O(n*d)\n");
-	
-	printf("\n");
 }
 
 void rotate_array(int *array,unsigned int d,unsigned int len)
@@ -40,20 +25,26 @@ void rotate_array(int *array,unsigned int d,unsigned int len)
 	{
 		return;
 	}
-
 	//Outer for loop will run for d times
 	for(i=0;i<d;i++)
 	{
-		
-		// {1,2,3,4,5}
     	// 1) Store first element in temp
     	// 2) Shift each element towards left
     	// 3) last element will be replaced with temp
 		temp = array[0];
-		for(j=0;j<len;j++)
+		for(j=0;j<len-1;j++)
 		{
 			array[j] = array[j+1];
 		}
 		array[j] = temp;
 	}
+}
+void print_array(int *array,unsigned int len)
+{
+	unsigned int i=0;
+	for(i=0;i<len;i++)
+	{
+		printf("%d ",array[i]);
+	}
+	printf("\n");
 }
